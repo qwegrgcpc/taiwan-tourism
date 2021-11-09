49b45f12f9cfaba6626e1032692219dde39dc546
HaHa <57859781+Viaje9@users.noreply.github.com>
2021-11-09 22:14:45 +0800

    設定正式環境publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,7 @@
 module.exports = {
-  chainWebpack: config => {
-    config
-    .plugin('html')
-    .tap(args => {
+  publicPath: process.env.NODE_ENV === 'production' ? 'taiwan-tourism/' : '/',
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
       args[0].title = '台灣哪裡趣'
       return args
     })
