494b206f08b8a4016297f3e380be3bccb3ac3d26
qwegrgcpc <67733887+qwegrgcpc@users.noreply.github.com>
2021-11-09 12:39:46 +0000

    Deploying to gh-pages from @ qwegrgcpc/taiwan-tourism@314f90143bb02471ef85eeeafb356ae5e50b59bb 🚀

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './assets/tailwind.css'
-import router from './router'
-import store from './store'
-import breakpoints from './plugins/breakpoints'
-
-createApp(App).use(store).use(router).use(breakpoints).mount('#app')
