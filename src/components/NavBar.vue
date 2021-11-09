d309643fcd2a7585e856958dc0de4148f08fb730
HAHA <brian15628@gmail.com>
2021-11-09 17:58:38 +0800

    新增breakpoints(未完成)

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,34 +1,37 @@
 <template>
-  <nav class="menu">
-    <div class="logo">
-      <img
-        class="lg:h-10 h-7"
-        src="@/assets/images/logo.svg"
-        alt="台灣哪裡趣"
-      />
-    </div>
-    <div class="block lg:hidden">
-      <button class="py-2" @click="isOpen = !isOpen">
-        <img class="h-5" :src="require(`@/assets/images/${menuIcon}.svg`)" />
-      </button>
-    </div>
-    <div class="flex lg:w-auto w-full justify-center">
-      <ul class="menu_list">
-        <li class="search_btn">
-          <router-link to="/">景點 </router-link>
-        </li>
-        <li class="search_btn">
-          <router-link to="/">旅宿</router-link>
-        </li>
-        <li class="search_btn">
-          <router-link to="/">餐飲</router-link>
-        </li>
-        <li class="itinerary_btn">
-          <router-link class="vertical-center" to="/">
-            <i><img src="@/assets/images/journey.svg" /></i> 自訂行程
-          </router-link>
-        </li>
-      </ul>
+  <nav class="nav" :class="{ active: $breakpoint.isSmaller('lg') && isOpen }">
+    <div class="nav_area">
+      <div class="vertical-center">
+        <img
+          class="lg:h-10 h-7"
+          src="@/assets/images/logo.svg"
+          alt="台灣哪裡趣"
+        />
+      </div>
+      {{ $breakpoint.isSmaller('lg') }}
+      <div class="block lg:hidden">
+        <button class="py-2" @click="isOpen = !isOpen">
+          <img class="h-5" :src="require(`@/assets/images/${menuIcon}.svg`)" />
+        </button>
+      </div>
+      <div v-show="showMenuList" class="flex lg:w-auto w-full justify-center">
+        <ul class="menu_list">
+          <li class="search_btn">
+            <router-link to="/">景點 </router-link>
+          </li>
+          <li class="search_btn">
+            <router-link to="/">旅宿</router-link>
+          </li>
+          <li class="search_btn">
+            <router-link to="/">餐飲</router-link>
+          </li>
+          <li class="itinerary_btn">
+            <router-link class="vertical-center" to="/">
+              <i><img src="@/assets/images/journey.svg" /></i> 自訂行程
+            </router-link>
+          </li>
+        </ul>
+      </div>
     </div>
   </nav>
 </template>
@@ -43,6 +46,9 @@ export default {
   computed: {
     menuIcon() {
       return this.isOpen ? 'menuClose' : 'menu'
+    },
+    showMenuList() {
+      return this.$breakpoint.is !== 'min' || this.isOpen
     }
   }
 }
@@ -53,26 +59,37 @@ export default {
   display: flex;
   align-items: center;
 }
-.menu {
-  @apply flex justify-between items-center px-5 flex-wrap bg-white fixed w-full h-full;
+.nav {
+  @apply sticky top-0 bg-white w-full;
 }
-
+.nav.active {
+  @apply fixed h-full;
+}
+.nav_area {
+  @apply flex justify-between px-5 flex-wrap;
+}
+.search_btn {
+  @apply text-xl flex justify-center text-j-black-900 font-bold mt-10 border-b-2 divide-j-black-100;
+}
 .itinerary_btn {
-  @apply rounded-full text-xl text-white bg-j-black-900 py-2 px-5;
+  @apply rounded-full text-xl text-white bg-j-black-900 py-2 px-5 mt-12;
 }
 
 @screen lg {
-  .menu {
-    @apply px-10 h-24 sticky top-0;
+  .nav {
+    @apply px-10 sticky top-0;
   }
-  .menu_list {
-    @apply lg:flex-row;
+  .nav_area {
+    @apply px-10 h-24;
   }
   .menu_list {
     @apply flex lg:flex-row flex-col items-center;
   }
   .search_btn {
-    @apply text-xl text-j-blue-300 font-bold mr-10 vertical-center;
+    @apply text-j-blue-300 mr-10 vertical-center mt-0 border-0;
+  }
+  .itinerary_btn {
+    @apply mt-0;
   }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue'
 import './assets/tailwind.css'
 import router from './router'
 import store from './store'
+import breakpoints from './plugins/breakpoints'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(breakpoints).mount('#app')
