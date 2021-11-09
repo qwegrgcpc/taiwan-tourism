87a1718f34193efa026bdcbf00f4172751d20fdf
HaHa <76926830+NeptunusDio@users.noreply.github.com>
2021-11-09 22:19:29 +0800

    publicPath測試

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? 'taiwan-tourism/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/taiwan-tourism/' : '/',
   chainWebpack: (config) => {
     config.plugin('html').tap((args) => {
       args[0].title = '台灣哪裡趣'
