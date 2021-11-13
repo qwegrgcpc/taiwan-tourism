b9438e08e4f0256d19bcd2c4c225135695ca483c
qwegrgcpc <67733887+qwegrgcpc@users.noreply.github.com>
2021-11-14 03:51:01 +0800

    首頁banner部分

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -12,146 +12,151 @@
         </li>
       </ul>
     </div>
-    <div class="flex">
-      <div class="flex-auto mt-4">
-        <p class="bold_text">縣市</p>
-        <div class="flex justify-between w-full">
-          <select v-model="searchParams.city" class="input items-center w-full">
-            <option v-for="city in option.city" :key="city" :value="city">
-              {{ city }}
-            </option>
-          </select>
-          <i class="dropdown_icon">
-            <img :src="require(`@/assets/images/dropdown.svg`)" />
-          </i>
+    <div class="tabContent">
+      <div class="inputArea">
+        <div>
+          <p>縣市</p>
+          <div>
+            <select v-model="searchParams.city" class="input">
+              <option v-for="city in option.city" :key="city" :value="city">
+                {{ city }}
+              </option>
+            </select>
+            <i class="dropdown_icon">
+              <img :src="require(`@/assets/images/dropdown.svg`)" />
+            </i>
+          </div>
+        </div>
+        <div>
+          <p>類別</p>
+          <div>
+            <select v-model="searchParams.category" class="input">
+              <option value="">請選擇類別</option>
+              <option
+                v-for="option in option[searchParams.tab]"
+                :key="option"
+                :value="option"
+              >
+                {{ option }}
+              </option>
+            </select>
+            <i class="dropdown_icon">
+              <img :src="require(`@/assets/images/dropdown.svg`)" />
+            </i>
+          </div>
+        </div>
+        <div>
+          <p>關鍵字</p>
+          <div class="pr-4">
+            <input
+              v-model="searchParams.keyword"
+              type="text"
+              class="input"
+              placeholder="請輸入關鍵字"
+            />
+          </div>
         </div>
       </div>
-      <div class="flex-auto mt-4">
-        <p class="bold_text">類別</p>
-        <div class="flex justify-between w-full">
-          <select
-            v-model="searchParams.category"
-            class="input items-center w-full"
-          >
-            <option value="">請選擇類別</option>
-            <option
-              v-for="option in option[searchParams.tab]"
-              :key="option"
-              :value="option"
-            >
-              {{ option }}
-            </option>
-          </select>
-          <i class="dropdown_icon">
-            <img :src="require(`@/assets/images/dropdown.svg`)" />
-          </i>
-        </div>
+      <div class="searchArea">
+        <div class="search_btn" @click="search">搜尋</div>
       </div>
-      <div class="flex-auto mt-4">
-        <p class="bold_text">關鍵字</p>
-        <div class="flex justify-between w-full">
-          <input
-            v-model="searchParams.keyword"
-            type="text"
-            class="input items-center w-full"
-            placeholder="請輸入關鍵字"
-          />
-        </div>
-      </div>
-      <div class="search_btn" @click="search">搜尋</div>
     </div>
   </div>
 </template>
 <script>
-import { option } from '@/assets/json/options.json'
+import { option } from "@/assets/json/options.json";
 import {
   fetchScenicSpotAll,
   fetchRestaurantAll,
-  fetchHotelAll
-} from '@/apis/tourism'
+  fetchHotelAll,
+} from "@/apis/tourism";
 export default {
   data() {
     return {
       option,
       tabList: [
-        { name: '景點', value: 'scenicSpot' },
-        { name: '旅宿', value: 'hotel' },
-        { name: '餐飲', value: 'restaurant' }
+        { name: "景點", value: "scenicSpot" },
+        { name: "旅宿", value: "hotel" },
+        { name: "餐飲", value: "restaurant" },
       ],
       searchParams: {
-        tab: 'scenicSpot',
-        city: '全部',
-        category: '全部',
-        keyword: ''
-      }
-    }
+        tab: "scenicSpot",
+        city: "全部",
+        category: "全部",
+        keyword: "",
+      },
+    };
   },
   computed: {
     searchData() {
-      return this.$store.state.searchData.filter((_, i) => i < 10)
+      return this.$store.state.searchData.filter((_, i) => i < 10);
     },
     apiParams() {
-      const { city, category, keyword } = this.searchParams
-      let $filter = ''
-      const addParams = () => ($filter ? ' and' : '')
+      const { city, category, keyword } = this.searchParams;
+      let $filter = "";
+      const addParams = () => ($filter ? " and" : "");
 
-      if (city !== '全部') {
-        $filter += `City eq '${city}'`
+      if (city !== "全部") {
+        $filter += `City eq '${city}'`;
       }
 
-      if (category !== '全部') {
-        const ClassEqStr = `Class1 eq '${category}' or Class1 eq '${category}' or Class1 eq '${category}'`
-        $filter += `${addParams()} (${ClassEqStr})`
+      if (category !== "全部") {
+        const ClassEqStr = `Class1 eq '${category}' or Class1 eq '${category}' or Class1 eq '${category}'`;
+        $filter += `${addParams()} (${ClassEqStr})`;
       }
 
       if (keyword) {
         const keywordStr = (keyword) =>
-          ` contains(Name,'${keyword}') or contains(Address ,'${keyword}') or contains(DescriptionDetail,'${keyword}') or contains(Description,'${keyword}')`
+          ` contains(Name,'${keyword}') or contains(Address ,'${keyword}') or contains(DescriptionDetail,'${keyword}') or contains(Description,'${keyword}')`;
         const keywordList = keyword
-          .split(' ')
+          .split(" ")
           .reduce(
             (acc, cur, i, arr) =>
-              (acc += `${keywordStr(cur)} ${i !== arr.length - 1 ? 'or' : ''}`),
-            ''
-          )
-        $filter += `${addParams()} (${keywordList})`
+              (acc += `${keywordStr(cur)} ${i !== arr.length - 1 ? "or" : ""}`),
+            ""
+          );
+        $filter += `${addParams()} (${keywordList})`;
       }
 
-      return $filter ? { $filter } : {}
+      return $filter ? { $filter } : {};
     },
     fetchApi() {
-      const { tab } = this.searchParams
-      if (tab === 'scenicSpot') {
-        return fetchScenicSpotAll
+      const { tab } = this.searchParams;
+      if (tab === "scenicSpot") {
+        return fetchScenicSpotAll;
       }
-      if (tab === 'hotel') {
-        return fetchRestaurantAll
+      if (tab === "hotel") {
+        return fetchRestaurantAll;
       }
-      if (tab === 'restaurant') {
-        return fetchHotelAll
+      if (tab === "restaurant") {
+        return fetchHotelAll;
       }
-      return () => {}
-    }
+      return () => {};
+    },
   },
   methods: {
     changeTab(tab) {
-      this.searchParams.tab = tab
-      this.searchParams.class = '全部'
+      this.searchParams.tab = tab;
+      this.searchParams.class = "全部";
     },
     search() {
-      this.loading = true
+      this.loading = true;
       this.fetchApi(this.apiParams)
         .then(({ data }) => {
-          this.$store.commit('setSearchData', data)
+          this.$store.commit("setSearchData", data);
         })
         .finally(() => {
-          this.loading = false
-        })
-    }
-  }
-}
+          this.loading = false;
+        });
+    },
+  },
+};
 </script>
 <style scoped>
+.main {
+  @apply bg-white pt-4 pb-6 max-w-full pr-4;
+}
+
 .flex > li {
   @apply mr-10 bold_text py-1 px-2 cursor-pointer;
   @apply text-j-black-500;
@@ -161,25 +166,41 @@ export default {
   @apply bold_text z-20 border-j-orange border-b-4;
 }
 
-.tabArea::after {
-  content: '';
-  @apply absolute w-full left-0 transform -translate-y-0.5 h-0.5 bg-j-black-100 block;
+.tabArea {
+  @apply ml-8;
 }
 
-.main {
-  @apply left-20 rounded-2xl bg-white pt-4 pb-6 px-8 shadow-md;
+.tabArea::after {
+  content: "";
+  @apply absolute w-full -left-4 transform -translate-y-0.5 h-0.5 bg-j-black-100 block;
 }
 
 .bold_text {
   @apply font-bold text-xl text-j-black-900;
 }
 
-.flex > .flex-auto {
-  @apply mr-10;
+.inputArea {
+  @apply flex-auto;
+}
+
+.inputArea > div {
+  @apply mt-4 w-full flex;
+}
+
+.inputArea > div > p {
+  @apply bold_text w-20 self-center;
+}
+
+.inputArea > div > div {
+  @apply flex flex-auto;
+}
+
+.searchArea {
+  @apply mt-4 pr-4;
 }
 
 .input {
-  @apply rounded-lg max-w-xl h-10 border border-gray-300 mt-3 py-1 px-3 cursor-pointer;
+  @apply rounded-lg max-w-xl h-10 border border-gray-300 py-1 px-3 cursor-pointer items-center w-full;
 }
 
 .input_text button,
@@ -191,10 +212,56 @@ textarea {
 }
 
 .search_btn {
-  @apply rounded-lg w-20 h-10 text-lg text-white text-center py-1 leading-8 self-end bg-j-orange cursor-pointer;
+  @apply rounded-lg w-full h-10 text-lg text-white text-center py-1 leading-8 self-end bg-j-orange cursor-pointer;
 }
 
 .dropdown_icon {
-  @apply relative transform -translate-x-7 translate-y-6 z-40;
+  @apply relative transform -translate-x-7 translate-y-4 z-40;
+}
+
+@screen lg {
+  .main {
+    @apply shadow-md rounded-2xl px-8;
+  }
+
+  .tabArea {
+    @apply ml-0;
+  }
+
+  .tabArea::after {
+    @apply left-0;
+  }
+
+  .tabContent {
+    @apply flex;
+  }
+
+  .inputArea {
+    @apply flex;
+  }
+
+  .inputArea > div {
+    @apply w-1/3 flex-wrap;
+  }
+
+  .inputArea > div > p {
+    @apply w-full;
+  }
+
+  .inputArea > div > div {
+    @apply w-full;
+  }
+
+  .input {
+    @apply mt-3;
+  }
+
+  .searchArea {
+    @apply mt-auto pr-0;
+  }
+
+  .search_btn {
+    @apply w-20;
+  }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,24 @@
 <template>
   <div class="home">
     <div class="wrapper">
-      <SearchBar class="searchBar" />
-      <div class="img">
-        <img class="bg" :src="require(`@/assets/images/bg.svg`)" />
+      <div class="bg">
+        <div class="w-1/12 mr-3"></div>
+        <div class="w-2/12 mr-3"></div>
+        <div class="w-3/12"></div>
+      </div>
+      <div class="banner">
+        <img src="@/assets/images/photoScenicSpot.jpg" />
+        <div class="banner_intro">
+          <p class="banner_title">景點快搜</p>
+          <p class="banner_subtitle">在頂溪，找到令你怦然心動的風景</p>
+        </div>
+        <SearchBar v-if="true" class="searchBar" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import SearchBar from "@/components/SearchBar.vue";
 
 export default {
@@ -22,29 +30,72 @@ export default {
 </script>
 <style scoped>
 .wrapper {
-  @apply flex justify-end mx-auto px-20;
+  @apply flex justify-end mx-auto px-4;
 }
 
-.img {
-  /* background-color: blue;  */
-  width: 990px;
-  height: 340px;
-  z-index: 3;
+.banner {
+  @apply w-full z-10;
+}
+
+.banner > img {
+  @apply h-60 w-full object-cover;
+}
+
+.banner_intro {
+  @apply h-20 p-3 w-full bg-j-black-900 relative transform -translate-y-20 opacity-80;
+}
+
+.banner_title {
+  @apply text-xl text-white;
+}
+
+.banner_subtitle {
+  @apply text-sm text-white;
 }
 
 .bg {
-  /* font-size: 20px; */
-  width: 340px;
-  height: 990px;
-  position: absolute;
-  z-index: 2;
-  @apply text-lg;
+  @apply absolute right-0 flex justify-end h-64 w-full;
 }
+
+.bg > div {
+  @apply bg-j-blue-100 h-full;
+}
+
 .searchBar {
-  position: absolute;
-  top: 170px;
-  width: 938px;
-  height: 175px;
-  z-index: 5;
+  @apply absolute z-40 text-lg w-full -mt-12;
+}
+
+@screen lg {
+  .wrapper {
+    @apply px-8;
+  }
+
+  .banner > img {
+    height: 640px;
+    @apply w-8/12 ml-auto;
+  }
+
+  .banner_intro {
+    @apply absolute bg-none mt-24 opacity-100 bg-opacity-0;
+    top: 240px;
+  }
+
+  .banner_title {
+    @apply text-4xl font-bold text-j-black-900;
+  }
+
+  .banner_subtitle {
+    @apply text-2xl text-j-black-500;
+  }
+
+  .searchBar {
+    @apply absolute z-40 text-lg mt-24 w-1/2;
+    top: 368px;
+  }
+
+  .bg{
+    height: 640px;
+    @apply mt-10
+  }
 }
 </style>
